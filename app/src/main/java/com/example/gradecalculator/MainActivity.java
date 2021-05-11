cab78648d802e9b227bb34027d290b2a0d9035fd
JungYeonHee <dusgml5328@gmail.com>
2021-05-12 01:30:37 +0900

    feat: add volly using singleton

--- app/src/main/java/com/example/gradecalculator/MainActivity.java
+++ app/src/main/java/com/example/gradecalculator/MainActivity.java
@@ -1,14 +1,47 @@
 package com.example.gradecalculator;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+
 
 public class MainActivity extends AppCompatActivity {
+    private TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv = findViewById(R.id.tvMain);
+
+        String url = "http://www.google.com";
+
+        // Formulate the request and handle the response.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+
+            new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    tv.setText("Response is: "+ response.substring(0,500));
+                }
+            }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                tv.setText("That didn't work!");
+            }
+        });
+
+        // Get a RequestQueue && Add a request (in this example, called stringRequest) to your RequestQueue.
+        MySingleton.getInstance(this).addToRequestQueue(stringRequest);
+
     }
+
 }
