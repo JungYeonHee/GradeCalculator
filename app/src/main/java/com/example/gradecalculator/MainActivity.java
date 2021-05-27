c8d0bc812cb6097868e3d140aa96d4890dadafc6
wldnjs3865 <78153919+wldnjs3865@users.noreply.github.com>
2021-05-28 03:13:49 +0900

    Linechart

--- app/src/main/java/com/example/gradecalculator/MainActivity.java
+++ app/src/main/java/com/example/gradecalculator/MainActivity.java
@@ -1,12 +1,7 @@
 package com.example.gradecalculator;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
-
 import android.os.Bundle;
-import android.view.MenuItem;
 import android.widget.TextView;
 
 import com.android.volley.Request;
@@ -15,31 +10,17 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
-import com.example.gradecalculator.graduation.GraduationFragment;
-import com.example.gradecalculator.home.HomeFragment;
-import com.example.gradecalculator.management.ManagementFragment;
-import com.example.gradecalculator.mypage.MyPageFragment;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-import org.jetbrains.annotations.NotNull;
 
 
 public class MainActivity extends AppCompatActivity {
     private TextView tv;
 
-    private BottomNavigationView bottomNavigationView; //바텀 네비게이션 뷰
-    private FragmentManager fm;
-    private FragmentTransaction ft;
-    private HomeFragment frag1;
-    private ManagementFragment frag2;
-    private GraduationFragment frag3;
-    private MyPageFragment frag4;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-       /* tv = findViewById(R.id.tvMain);
+        tv = findViewById(R.id.tvMain);
 
         String url = "http://www.google.com";
 
@@ -60,60 +41,7 @@ public class MainActivity extends AppCompatActivity {
 
         // Get a RequestQueue && Add a request (in this example, called stringRequest) to your RequestQueue.
         MySingleton.getInstance(this).addToRequestQueue(stringRequest);
-*/
-        bottomNavigationView = findViewById(R.id.bottomNavi);
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull @NotNull MenuItem menuItem) {
-                switch (menuItem.getItemId()) {
-                    case R.id.bottom_home:
-                        setFrag(0);
-                        break;
-                    case R.id.bottom_management:
-                        setFrag(1);
-                        break;
-                    case R.id.bottom_graduation:
-                        setFrag(2);
-                        break;
-                    case R.id.bottom_mypage:
-                        setFrag(3);
-                        break;
-                }
-                return false;
-            }
-        });
 
-        bottomNavigationView = findViewById(R.id.bottomNavi);
-        frag1 = new HomeFragment();
-        frag2 = new ManagementFragment();
-        frag3 = new GraduationFragment();
-        frag4 = new MyPageFragment();
-
-        setFrag(0); //첫 프래그먼트 화면을 무엇으로 지정해줄 것인지 결정
-    }
-
-    //프래그먼트 교체가 일어나는 실행문이다.
-    private void setFrag(int n) {
-        fm = getSupportFragmentManager();
-        ft = fm.beginTransaction();
-        switch (n) {
-            case 0:
-                ft.replace(R.id.main_frame, frag1);
-                ft.commit();
-                break;
-            case 1:
-                ft.replace(R.id.main_frame, frag2);
-                ft.commit();
-                break;
-            case 2:
-                ft.replace(R.id.main_frame, frag3);
-                ft.commit();
-                break;
-            case 3:
-                ft.replace(R.id.main_frame, frag4);
-                ft.commit();
-                break;
-        }
     }
 
 }
--- app/src/main/java/com/example/gradecalculator/home/HomeFragment.java
+++ app/src/main/java/com/example/gradecalculator/home/HomeFragment.java
@@ -1,16 +1,49 @@
 package com.example.gradecalculator.home;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.fragment.app.Fragment;
+
 import com.example.gradecalculator.R;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+
+import java.util.ArrayList;
 
 public class HomeFragment extends Fragment {
+
+    private LineChart lineChart;
+
+    private ArrayList<ILineDataSet> DataSets = new ArrayList<>();
+    private LineDataSet lineDataSet;
+    private ArrayList<Entry> totalGrade = new ArrayList<>();
+/* 전공 그래프
+   private ArrayList<Entry> majorGrade = new ArrayList<>();*/
+    private LineData data = new LineData(DataSets);
+
+    public void sendInput(float value) {
+
+        data = lineChart.getData();
+        ILineDataSet xSet = data.getDataSetByIndex(0);
+
+        data.addEntry(new Entry(xSet.getEntryCount(), value),0);
+
+        //lineDataSet5.notifyDataSetChanged();
+        //lineDataSets.add(lineDataSet5);
+        data.notifyDataChanged();
+        lineChart.notifyDataSetChanged();
+        lineChart.invalidate();
+        lineChart.moveViewToX(data.getEntryCount());
+    }
 
     // TODO: Rename parameter arguments, choose names that match
 
@@ -25,14 +58,36 @@ public class HomeFragment extends Fragment {
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
-
-    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        super.onCreate(savedInstanceState);
+        // setContentView(R.layout.activity_main);
+        View view = inflater.inflate(R.layout.activity_main,container,false);
+        lineChart = (LineChart) getView().findViewById(R.id.linechart);
+      //x축
+        ArrayList<String> xLabel = new ArrayList<>();
+        xLabel.add("1-1"); xLabel.add("1-2");
+        xLabel.add("2-1"); xLabel.add("2-2");
+        xLabel.add("3-1"); xLabel.add("3-2");
+        xLabel.add("4-1"); xLabel.add("4-2");
+
+        XAxis xAxis = lineChart.getXAxis();
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setDrawGridLines(false);
+        xAxis.setTextSize(10f);
+        xAxis.setDrawGridLines(true);
+        xAxis.setValueFormatter(new IndexAxisValueFormatter(){
+           public String getFormattedValue(float value, AxisBase axis){
+               return xLabel.get((int)value);
+           }
+        });
+
+
         return inflater.inflate(R.layout.fragment_home, container, false);
+
+
     }
+
+
 }
